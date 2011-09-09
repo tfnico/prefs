43993ccbe9ddda356a95242ee934f3cd0110f244
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-09-09 14:16:58 +0200

    Oh my zsh with zshrc

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 cp -i -v gitconfig ~/.gitconfig
 cp -i -v vimrc ~/.vimrc
 cp -i -v profile ~/.profile
+cp -i -v zshrc ~/.zshrc
 cp -i -v subversion-config ~/.subversion/config
 #cp -R vim/ ~/.vim
 cp -v -R bin-source/ ~/bin
