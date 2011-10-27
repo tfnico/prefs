abd7be0677d2c7f7504a7d288537523413aba3fc
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-10-27 08:40:46 +0200

    Initial reverse deploy

---
+++ reverse-deploy.sh
@@ -0,0 +1,9 @@
+#!
+echo "If you are at work make sure that the git-email remains the work mail!!"
+cp -i -v ~/.gitconfig gitconfig 
+cp -i -v ~/.vimrc vimrc 
+cp -i -v ~/.profile profile 
+cp -i -v ~/.zshrc zshrc 
+cp -i -v ~/.subversion/config subversion-config
+#cp -R vim/ ~/.vim
+cp -v -R ~/bin/* bin-source
