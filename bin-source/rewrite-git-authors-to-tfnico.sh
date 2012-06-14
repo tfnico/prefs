300256861218ec22cec80fac50750a68aca8ea3b
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2012-06-14 12:52:40 +0200

    Improve rewrite authors script
    Used for flurfunk history rewrite.

--- bin-source/rewrite-git-authors-to-tfnico.sh
+++ bin-source/rewrite-git-authors-to-tfnico.sh
@@ -1,11 +1,26 @@
-git filter-branch --commit-filter '
-        if [ "$GIT_COMMITTER_NAME" = "Thomas Ferris Nicolaisen" ];
-        then
-                GIT_COMMITTER_NAME="Thomas Ferris Nicolaisen";
-                GIT_AUTHOR_NAME="Thomas Ferris Nicolaisen";
-                GIT_COMMITTER_EMAIL="tfnico@gmail.com";
-                GIT_AUTHOR_EMAIL="tfnico@gmail.com";
-                git commit-tree "$@";
-        else
-                git commit-tree "$@";
-        fi' HEAD
+export OLD_EMAIL="thomas.nicolaisen@viaboxx.de"
+export NEW_NAME="Thomas Ferris Nicolaisen"
+export NEW_EMAIL="tfnico@gmail.com"
+
+git filter-branch --env-filter '
+an="$GIT_AUTHOR_NAME"
+am="$GIT_AUTHOR_EMAIL"
+cn="$GIT_COMMITTER_NAME"
+cm="$GIT_COMMITTER_EMAIL"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    cn="$NEW_NAME"
+    cm="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    an="$NEW_NAME"
+    am="$NEW_EMAIL"
+fi
+
+export GIT_AUTHOR_NAME="$an"
+export GIT_AUTHOR_EMAIL="$am"
+export GIT_COMMITTER_NAME="$cn"
+export GIT_COMMITTER_EMAIL="$cm"
+'
